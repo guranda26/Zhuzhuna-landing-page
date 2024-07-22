f070ceed92f7cb2fbf2a0bc4e86119f00fe53b92
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-07-22 04:24:14 +0400

    add curves

--- src/pages/Wines.jsx
+++ src/pages/Wines.jsx
@@ -6,35 +6,40 @@ const Wines = () => {
   return (
     <Sections id="wines" title="ბოთლი">
       <div className="relative flex center md:justify-between h-auto border-2 border-buttonColor-primary">
-        <div className="max-w-[50%] lg:w-auto px-3 py-5 md:p-0">
-          <img
-            src="../../public/images/bottle1.png"
-            alt="bottle"
-            className=""
-          />
+        <div className="max-w-[50%] lg:w-auto px-3 py-5 md:p-0 z-20">
+          <img src="../../public/images/bottle1.png" alt="bottle" />
           <p className="md:hidden">
             ჟუჟუს ბოთლი 250 მლ-იანია, რადგან სწორედ ეს პორცია ინარჩუნებს
             მისართმევად საუკეთესო ტემპერატურასა და გაზს.
           </p>
         </div>
-        <article className="relative md:w-[65%] sm:w-[60%] flex flex-col px-2 lg:px-5">
+        <article className="md:w-[65%] sm:w-[60%] flex flex-col px-2 lg:px-5">
+          <div className="absolute top-0 right-0 w-full h-full overflow-hidden z-10">
+            <svg
+              viewBox="0 0 800 150"
+              preserveAspectRatio="none"
+              className="absolute top-0 right-0 w-full h-full"
+            >
+              <path
+                d="M0,50 C150,150 300,40 450, 100 C600,150 750,50 900,100 C1050,150 1200,50 1200,50 L1200,150 L0,150 Z"
+                className="fill-backgroundColor-purpleStart "
+              />
+            </svg>
+          </div>
           <div className="py-3 md:py-7 flex flex-col gap-10 items-center md:items-start">
             <h3 className="font-tommaso text-4xl">ჟუჟუნა ბოთლი</h3>
             <div className="flex md:flex-row flex-col gap-6 justify-items-start">
-              <WineDetails description="მოცულობა" info="250 მლ"></WineDetails>
-              <WineDetails description="ალკოჰოლი" info="11 %"></WineDetails>
-              <WineDetails
-                description="ბუნებრივი გაზი"
-                info="2.5 ბარი"
-              ></WineDetails>
+              <WineDetails description="მოცულობა" info="250 მლ" />
+              <WineDetails description="ალკოჰოლი" info="11 %" />
+              <WineDetails description="ბუნებრივი გაზი" info="2.5 ბარი" />
             </div>
-            <p className="md:block hidden md:text-xl lg:text-2xl">
+            <p className="md:block hidden md:text-xl lg:text-2xl z-20">
               ჟუჟუს ბოთლი 250 მლ-იანია, რადგან სწორედ ეს პორცია ინარჩუნებს
               მისართმევად საუკეთესო ტემპერატურასა და გაზს.
             </p>
           </div>
-          <div className="md:absolute right-20 md:right-10 md:bottom-[-50] lg:bottom-5 px-5 ml-auto">
-            <Price></Price>
+          <div className="md:absolute right-20 md:right-10 md:bottom-[-50] lg:bottom-5 px-5 ml-auto z-20">
+            <Price />
           </div>
         </article>
       </div>
